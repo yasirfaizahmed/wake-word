e4d42c3450a5060411ad7b7aeaf1d4247cc20a17
xd <yasirfaizahmed.n@gmail.com>
2025-10-11 19:35:12 +0530

    all coompleted

--- utils/config.py
+++ utils/config.py
@@ -17,7 +17,10 @@ NON_WAKE_CHOPPED_DIR = p.Path.joinpath(RECORDINGS_PATH, "non-wake-chopped")
 SAMPLE_RATE = 16000   # per second
 NUM_RECORDINGS = 3
 HARDWARE = "0,0"
-DURATION = 3        # seconds
+DURATION = 2        # seconds
 SAMPLE_RATE = 44100
 N_MFCC = 20        # number of features for MFCC to capture
 N_AUG_PER_FILE = 30     # how many augmented variants per original
+# librosa default hop_len = 512, and with sr=16000 frame_rate = 16000/512 ~= 31
+# and since our duration of each audio file is 2 seconds, hence MAX_LEN = 2 * 31 ~ 64
+MAX_LEN = 64
